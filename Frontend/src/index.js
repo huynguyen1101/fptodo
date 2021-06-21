78716d7846d310a32f220b18e5b5fdde136bfed7
thaomai98 <82597595+thaomai98@users.noreply.github.com>
2021-06-21 16:27:09 +0700

    Implement login, register , home

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/app/App';
-import reportWebVitals from './reportWebVitals';
+import './static/css/index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+import { BrowserRouter } from "react-router-dom";
+import GlobalProvider from "./context/GlobalProvider";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  	<BrowserRouter>
+      <GlobalProvider>
+   		 <App />
+   	  </GlobalProvider>
+   	</BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
