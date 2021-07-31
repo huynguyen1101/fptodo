7a147f92d36edba6fbb25976192db2c9c0c394a5
quytra93 <quytra187@gmail.com>
2021-07-31 19:43:31 +0700

    view noti

--- Frontend/src/pages/User.js
+++ Frontend/src/pages/User.js
@@ -173,13 +173,13 @@ const User = () => {
           addProject={addProject}
         />
       )}
-      {showAddBoardModal && (
+      {/* {showAddBoardModal && (
         <AddBoardModal
           setShowAddBoardModal={setShowAddBoardModal}
           addBoard={addBoard}
           project={boardProject}
         />
-      )}
+      )} */}
 
     </>
   );
