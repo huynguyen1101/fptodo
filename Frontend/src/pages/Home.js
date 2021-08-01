74dada9b721daa2340f675d1b2ffb5f791943cc0
Kien Le <kienlntde130026@fpt.edu.vn>
2021-08-01 21:44:29 +0700

    remove duplicate

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -15,8 +15,6 @@ const Home = () => {
     const [showAddBoardModal, setShowAddBoardModal] = useState(false);
     const [boardProject, setBoardProject] = useState(0); // If 0, we are making a personal board. Else, making board for project with given ID
     const [showTeamModal, setShowTeamModal] = useState(false);
-    const { data: projects, addItem: addProject } = useAxiosGet("/projects/");
-    console.log(projects);
     let { data: projects, addItem: addProject } = useAxiosGet("/projects/");
     const {
         data: boards,
